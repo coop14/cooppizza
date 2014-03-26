e2a016099585749d8512e492cf0815537c4ca363
Baracasfig <lufig@usp.br>
2014-03-26 16:48:43 -0300

    mudanca nos settings.py

--- cooppizza/settings.py
+++ cooppizza/settings.py
@@ -10,7 +10,13 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from os.path import join
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+TEMPLATE_DIRS = (
+    join(BASE_DIR,  'cooppizza/templates'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -33,9 +39,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'clientes',
-    'cardapio',
-    'formFeedback',
+    'cooppizza.clientes',
+    'cooppizza.cardapio',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -64,7 +69,7 @@ DATABASES = {
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
 LANGUAGE_CODE = 'en-gb'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
@@ -75,7 +80,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-    'static/',
 )
-
-TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'cardapio/templates/cardapio/')]
