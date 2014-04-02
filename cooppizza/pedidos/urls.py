f0b6ee17ee7efb58c1f6987d98c7e938da266824
coop14 <anselmo.strufaldi@usp.br>
2014-04-02 16:16:56 -0300

    Módulo de Pedidos

--- cooppizza/pedidos/urls.py
+++ cooppizza/pedidos/urls.py
@@ -4,4 +4,6 @@ from cooppizza.pedidos import views
 urlpatterns = patterns('',
   url(r'^$', views.index, name='index'),
   url(r'^web$', views.web, name='web'), # WEB
+  url(r'^lista$', views.pedidoLista, name='pedidoLista'),
+  url(r'^novo$', views.pedidoNovo, name='pedidoNovo'),
 )
--- cooppizza/pedidos/views.py
+++ cooppizza/pedidos/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.template import RequestContext, loader
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, Http404
+from django.core.exceptions import PermissionDenied
 
 def web(request):
   template = loader.get_template('pedidos/indexWeb.html')
@@ -7,3 +11,16 @@ def web(request):
 def index(request):
   template = loader.get_template('pedidos/index.html')
   return HttpResponse(template.render(RequestContext(request)))
+
+def pedidoLista(request):
+  template = loader.get_template('pedidos/index.html')
+  return HttpResponse(template.render(RequestContext(request)))
+
+def pedidoNovo(request):
+  if 'id' in request.COOKIES:
+    cookie_id = request.COOKIES['id']
+    return HttpResponse('Got cookie with id=%s' % cookie_id)
+  else:
+    resp = HttpResponse('No id cookie! Sending cookie to client')
+    resp.set_cookie('id', 'some_value_99')
+    return resp
