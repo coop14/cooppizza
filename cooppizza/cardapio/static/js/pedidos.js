f1aa3109be76f694fed4c480bfca8a8a6c339388
coop14 <anselmo.strufaldi@usp.br>
2014-04-16 02:59:36 -0300

    Pedidos

--- cooppizza/cardapio/static/js/pedidos.js
+++ cooppizza/cardapio/static/js/pedidos.js
@@ -1,3 +1,11 @@
 $(function() {
   $('#dialog_pizza').show();
 });
+
+$(function() {
+  $('.pedido_pizza_quantidade').find('option').each(function() {
+    if ($(this).text() == $(this).parent().data('quantidade')) {
+      $(this).attr('selected', 'selected');
+    }
+  });
+});
