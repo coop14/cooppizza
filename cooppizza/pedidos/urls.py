cc32cd5e6addf967d26a3d0415143ea95c17de50
Baracasfig <lufig@usp.br>
2014-04-15 21:41:22 -0300

    tela do pedido

--- cooppizza/pedidos/urls.py
+++ cooppizza/pedidos/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, include, url
 from cooppizza.pedidos import views
 
 urlpatterns = patterns('',
-  url(r'^$', views.index, name='index'),
-  url(r'^web$', views.web, name='web'), # WEB
+  	url(r'^$', views.indexPedido, name='indexPedido'),
+  	url(r'^pizzas$', views.pizzasPedido, name='pizzasPedido'),
+  	url(r'^bebidas$', views.bebidasPedido, name='bebidasPedido'),
+  	url(r'^bebidas/adiciona/(?P<bebida_id>\d+)$', views.bebidaAdiciona, name='bebidaAdiciona'),
+  	url(r'^carrinho/(?P<produto_id>\d+)$', views.carrinhoPedido, name='carrinhoPedido'),
 )
--- cooppizza/pedidos/views.py
+++ cooppizza/pedidos/views.py
@@ -1,9 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.template import RequestContext, loader
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, Http404
+from django.core.exceptions import PermissionDenied
 
-def web(request):
-  template = loader.get_template('pedidos/indexWeb.html')
+from cooppizza.cardapio.models import Produto, Pizza, Ingrediente, Bebida, PizzaIngrediente, Item
+
+def indexPedido(request):
+  template = loader.get_template('indexPedido.html')
   return HttpResponse(template.render(RequestContext(request)))
 
-def index(request):
-  template = loader.get_template('pedidos/index.html')
-  return HttpResponse(template.render(RequestContext(request)))
+def pizzasPedido(request):
+	try:
+		pizzas = Pizza.objects.all()
+	except (Pizza.DoesNotExist):
+		raise Http404
+	else: 
+		return render(request, 'pizzasPedido.html', {'pizzas':pizzas})
+
+def bebidasPedido(request):
+	try:
+		bebidas = Bebida.objects.all()
+	except (Bebida.DoesNotExist):
+		raise Http404
+	else: 
+		return render(request, 'bebidasPedido.html', {'bebidas':bebidas})
+
+def bebidaAdiciona(request, bebida_id):
+	if request.method == "POST":
+		bebida = Bebida.objects.get(pk=bebida_id)
+		produto = bebida.produto
+		item = Item(produto = produto,
+			quantidade = request.POST['quantidade'],
+    		preco = produto.preco,
+    		promocao = False,
+    		observacao = request.POST['observacao'])
+		item.save()
+		return redirect(reverse('ingrediente'))
+	else:
+		raise PermissionDenied
+
+def carrinhoPedido(request, produto_id):
+	try:
+		produto_id = Produto.objects.get(pk=produto_id)
+	except (Produto.DoesNotExist):
+		raise Http404
+	else: 
+		return render(request, 'carrinhoPedido.html',
+		 {'produto':produto})
